1f5d63a39b6d795c3fc44778824d026976af870a
Mathias Fritsch <70696381+Matze9@users.noreply.github.com>
2022-09-15 12:37:05 +0200

    init

--- client/js/app.js
+++ client/js/app.js
@@ -1,6 +1,8 @@
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
-    .register("/sw.js")
+    .register(new URL("../sw.js", import.meta.url), { type: "module" })
     .then((reg) => console.log("Service Worker registered!", reg))
     .catch((err) => console.log("No Service Worker registered!", err));
 }
+
+console.log("got js");
--- sw.js
+++
@@ -1,3 +0,0 @@
-self.addEventListener("install", function (event) {
-  console.log("Service Worker was installed - note from the worker!");
-});
